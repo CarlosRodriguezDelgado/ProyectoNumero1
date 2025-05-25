90ee4462213c02465b92999871c880121b032e96
Gael Arce <gaelarce3@gmail.com>
2025-05-24 19:20:47 -0600

    Agregue la clase Singleton al proyecto

---
+++ Proyecto1/SingletonPuerto.java
@@ -0,0 +1,23 @@
+public class SingletonPuerto {
+     
+     private static SingletonPuerto instancia= null;
+
+    private static final int PUERTO= 5001;
+
+    private SingletonPuerto() {
+        
+    }
+
+    public  static synchronized SingletonPuerto getInstanceofPuerto(){
+        if (instancia==null) {
+         return instancia= new SingletonPuerto();
+        }
+            return instancia;
+        }
+    
+
+     public int getPuerto() {
+        return PUERTO;
+    }
+
+}
