e3099f769c87b2e2c8774b8ffc6d8b95f57892fc
Gael Arce <gaelarce3@gmail.com>
2025-05-27 01:09:31 -0600

    ya esta la estructura base

--- Proyecto1/ClientService.java
+++ Proyecto1/ClientService.java
@@ -1,3 +1,33 @@
-public class ClientService{
-    //Toda la logica del banco estará aca 
+public class ClientService implements Runnable{
+    //Toda la logica del banco estará aca import java.util.*;
+import java.io.*;
+import java.net.Socket;
+
+
+  private Socket clienteSocket=null;
+
+  public Manejador(Socket s) {
+    this.clienteSocket=s;
+  }
+
+    @Override
+    public void run() {
+        try (
+
+            // entradas y salidas
+           PrintWriter salida= new PrintWriter(clienteSocket.getOutputStream(), true);
+           BufferedReader entrada= new BufferedReader( new InputStreamReader(clienteSocket.getInputStream()));
+        ) {
+            
+            //primera salida  para avisar
+
+
+            //toda la logica
+
+        }catch(Exception e){
+          e.printStackTrace();
+        }
+    }
+
 }
+
