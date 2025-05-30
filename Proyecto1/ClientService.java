aa4e6a65736c81f51934fcb817d0ae445cb175d3
Evelyn Martinez <evemh19@gmail.com>
2025-05-29 21:13:44 -0600

    Correcciones: No debe ir el manejador en esta clase, el manejador se inicializa en el servidor, el manejador tiene que ser una clase aparte porque contiene la logica del menú y los mensajes que se mostraran en la app

--- Proyecto1/ClientService.java
+++ Proyecto1/ClientService.java
@@ -1,33 +1,8 @@
-public class ClientService implements Runnable{
+public class ClientService{
     //Toda la logica del banco estará aca import java.util.*;
-import java.io.*;
-import java.net.Socket;
 
 
-  private Socket clienteSocket=null;
-
-  public Manejador(Socket s) {
-    this.clienteSocket=s;
-  }
-
-    @Override
-    public void run() {
-        try (
-
-            // entradas y salidas
-           PrintWriter salida= new PrintWriter(clienteSocket.getOutputStream(), true);
-           BufferedReader entrada= new BufferedReader( new InputStreamReader(clienteSocket.getInputStream()));
-        ) {
-            
-            //primera salida  para avisar
-
-
-            //toda la logica
-
-        }catch(Exception e){
-          e.printStackTrace();
-        }
-    }
+ 
 
 }
 
